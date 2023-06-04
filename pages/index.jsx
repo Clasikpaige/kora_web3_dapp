a6a039451da3eb617c84b798a0cbe45d769d2fc7
Paige <102603571+Clasikpaige@users.noreply.github.com>
2023-06-04 23:46:54 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,13 @@
-import { useState } from "react";
+import React from "react";
 import homeStyles from "../styles/Home.module.css";
 import Container from "../components/container";
 import Button from "../components/button";
-import Input from "../components/Input";
-import containerStyles from "./Container.module.css";
-import buttonStyles from "./Button.module.css";
 
 export default function Home() {
-  const [coinBalance, setCoinBalance] = useState(0);
-  const [buyAmount, setBuyAmount] = useState(0);
-
-  const handleBuyCoins = () => {
-    // Logic for buying coins
-    // Update the coin balance and perform necessary operations
-    setCoinBalance(coinBalance + buyAmount);
-    setBuyAmount(0); // Reset the buy amount input field
+  const handleJoinWaitlist = () => {
+    // Logic for joining the waitlist
+    // Implement the necessary functionality here
+    console.log("Join waitlist");
   };
 
   return (
@@ -22,24 +15,16 @@ export default function Home() {
       <Container>
         <h1>Welcome to Kora Dapp</h1>
         <p>A high dynamic UI dApp for managing and trading coins.</p>
+        <p>Explore the world of decentralized finance with Kora.</p>
+        <p>Powered by Web3 technology.</p>
       </Container>
 
       <Container>
-        <h2>Buy Coins</h2>
-        <Input
-          type="number"
-          placeholder="Enter amount"
-          value={buyAmount}
-          onChange={(e) => setBuyAmount(parseInt(e.target.value))}
-        />
-        <Button className={buttonStyles.button} onClick={handleBuyCoins}>
-          Buy
+        <h2>Join the Waitlist</h2>
+        <p>Be the first to know when Kora is launched!</p>
+        <Button className={homeStyles.joinButton} onClick={handleJoinWaitlist}>
+          Join Waitlist
         </Button>
-      </Container>
-
-      <Container>
-        <h2>Coin Balance: {coinBalance}</h2>
-        {/* Display the user's coin balance */}
       </Container>
     </div>
   );
