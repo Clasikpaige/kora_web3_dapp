a8d1f2ab7033f7a0b6c24c82f3894ee4c66531fb
Paige <102603571+Clasikpaige@users.noreply.github.com>
2023-06-05 03:10:14 +0100

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -1,12 +1,13 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import styles from "../../styles/Navbar.module.css";
+
 export default function Navbar() {
-	return (
-		<nav className={styles.navbar}>
-			<a href="https://randomhathacking.hashnode.dev/?a=create-web3-dapp" target={"_blank"}>
-				<img className={styles.alchemy_logo} src="/logo.png"></img>
-			</a>
-			<ConnectButton></ConnectButton>
-		</nav>
-	);
+  return (
+    <nav className={styles.navbar}>
+      <a href="https://randomhathacking.hashnode.dev/?a=create-web3-dapp" target="_blank">
+        <img className={styles.alchemy_logo} src="https://www.dropbox.com/s/xesd76qc9wjs1oj/Photo%2005-06-2023%2C%202%2048%2024%20AM.jpg?raw=1" alt="Logo"></img>
+      </a>
+      <ConnectButton></ConnectButton>
+    </nav>
+  );
 }
