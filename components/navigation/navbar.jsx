67b45247a69c7448a55195ee098221b0ce08a050
Paige <102603571+Clasikpaige@users.noreply.github.com>
2023-06-05 03:03:18 +0100

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
 			<a href="https://randomhathacking.hashnode.dev/?a=create-web3-dapp" target={"_blank"}>
-				<img className={styles.alchemy_logo} src="/cw3d-logo.png"></img>
+				<img className={styles.alchemy_logo} src="/logo.png"></img>
 			</a>
 			<ConnectButton></ConnectButton>
 		</nav>
