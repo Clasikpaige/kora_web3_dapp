9552467fce5ab215c7b1fcbf37b44e5ad77b2833
Paige <102603571+Clasikpaige@users.noreply.github.com>
2023-06-07 21:03:07 +0100

    Create Bot.js

---
+++ pages/Bot.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page2 = () => {
+  return (
+    <div>
+      <h1>Page 2</h1>
+      {/* Add content specific to Page 2 */}
+    </div>
+  );
+};
+
+export default Page2;
