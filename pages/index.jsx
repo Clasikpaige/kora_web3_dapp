580546b9dce445a84545f34dfabd31ee8d737068
Paige <102603571+Clasikpaige@users.noreply.github.com>
2023-06-07 10:02:44 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,32 +17,31 @@ export default function Home() {
         <p className={styles.text}>A high dynamic UI dApp for managing and trading coins.</p>
         {showContent ? (
           <div>
-            <p className={styles.text}>Discover a new way to trade and invest in cryptocurrencies with Kora.</p>
-            <p className={styles.text}>
-              Join our waitlist to be the first to experience our innovative features:</p>
-            <ul className={styles.text}>
-              <li>Real-time market data and analytics</li>
-              <li>Secure and reliable transactions</li>
-              <li>Advanced trading tools and charts</li>
-              <li>Access to a wide range of cryptocurrencies</li>
-              <li>AI-powered bot for stock analysis</li>
-              <li>Personalized investment recommendations</li>
-            </ul>
-            <div className={styles.article}>
-              <p className={styles.text}>
-                At Kora, our vision is to empower individuals to take control of their financial future in the world of cryptocurrencies.
-                We strive to provide a user-friendly platform that simplifies the complexities of trading and investing, making it accessible to everyone.
-                With our real-time market data and advanced analytics, users can make informed decisions and stay ahead of the market trends.
-                Our secure and reliable transaction system ensures the safety of your assets, giving you peace of mind.
-                We believe in democratizing the world of cryptocurrencies, offering a wide range of options for users to explore and invest in.
-                Our AI-powered bot provides intelligent stock analysis, helping users make profitable trading decisions.
-                Additionally, our platform offers personalized investment recommendations based on individual goals and risk tolerance.
-                Join us on this exciting journey as we revolutionize the way people trade and invest in cryptocurrencies.
+            <div className={styles.featureContainer}>
+              <h2 className={styles.featureHeader}>Our Vision</h2>
+              <p className={styles.featureText}>
+                Kora is committed to empowering individuals to take control of their financial future through the power of
+                decentralized finance. We envision a world where everyone has equal access to innovative financial services
+                and the ability to participate in the global economy, regardless of their location or background.
               </p>
             </div>
-            <div className={styles.imageContainer}>
-              <img src="https://www.dropbox.com/s/7y5wnmefqwwc970/Photo%2007-06-2023%2C%209%2040%2045%20AM.jpg?raw=1" alt="Image 1" />
-              <img src="https://www.dropbox.com/s/qw3fs5jv6r6aj1n/Photo%2007-06-2023%2C%209%2042%2024%20AM.jpg?raw=1" alt="Image 2" />
+            <div className={styles.featureContainer}>
+              <h2 className={styles.featureHeader}>Our Features</h2>
+              <p className={styles.featureText}>
+                At Kora, we offer a comprehensive suite of features designed to provide a seamless and secure trading
+                experience. From real-time market data and advanced analytics to intuitive user interfaces and a wide range
+                of supported cryptocurrencies, we strive to meet the diverse needs of our users. Our platform is backed by
+                cutting-edge technology, ensuring fast and reliable transactions while maintaining the highest standards of
+                security.
+              </p>
+            </div>
+            <div className={styles.featureContainer}>
+              <h2 className={styles.featureHeader}>More</h2>
+              <p className={styles.featureText}>
+                With Kora, you can also take advantage of our AI-powered stock analysis tool, which provides valuable insights
+                and recommendations to help you make informed investment decisions. Our goal is to empower individuals with
+                the tools and knowledge needed to succeed in the dynamic world of cryptocurrency trading.
+              </p>
             </div>
           </div>
         ) : (
