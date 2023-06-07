606cacf1f3528e19bd0d096ab06a7776930959ef
Paige <102603571+Clasikpaige@users.noreply.github.com>
2023-06-07 21:39:04 +0100

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./Navbar";
-import bot from "./bot.js";
-import stock from "./stock.js";
-import buycoin from "./buycoin.js";
-import Dashboard from "./dashbord.js"; // Import the Dashboard component
+import styles from "./Navbar.module.css";
+import Navbar from "./navbar";
+import bot from "./bot";
+import stock from "./stock";
+import buycoin from "./buycoin";
+import Dashboard from "./dashbord"; // Import the Dashboard component
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/bot.js" component={Page1} />
-        <Route path="/stock.js" component={Page2} />
-        <Route path="/buycoin.js" component={Page3} />
-        <Route path="/dashboard.js" component={Dashboard} /> {/* Add the route for the Dashboard component */}
+        <Route path="/bot" component={Page1} />
+        <Route path="/stock" component={Page2} />
+        <Route path="/buycoin" component={Page3} />
+        <Route path="/dashboard" component={Dashboard} /> {/* Add the route for the Dashboard component */}
       </Switch>
     </Router>
   );
