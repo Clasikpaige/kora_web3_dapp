69c039cef7e74d5210a0c126860d37a70b6a4743
Paige <102603571+Clasikpaige@users.noreply.github.com>
2023-06-07 21:01:22 +0100

    Create Stock.js

---
+++ pages/Stock.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page1 = () => {
+  return (
+    <div>
+      <h1>Page 1</h1>
+      {/* Add content specific to Page 1 */}
+    </div>
+  );
+};
+
+export default Page1;
